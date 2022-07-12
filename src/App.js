9b4eff20b32387a397ac4d0d2020712c0553e276
hsbtr <hsbtr4@qq.com>
2022-07-13 00:14:53 +0800

    feat(global): 调试完成

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import { ProForm, ProFormItem, ProFormList, ProFormGroup, ProFormText, ProFormDependency } from "@ant-design/pro-form";
+import { Form, Space } from "antd";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ProForm>
+        <ProFormItem label={"测试"}>
+          <ProFormList name={"list"} initialValue={[{ label: undefined, value: undefined }]}>
+              <ProFormGroup>
+                  <ProFormText name={"label"} />
+                  <ProFormDependency name={["list", ["label"]]} ignoreFormListField={false}>
+                      {(depValue) => {
+                          console.log(depValue);
+                          return <span>{JSON.stringify(depValue)}</span>
+                      }}
+                  </ProFormDependency>
+              </ProFormGroup>
+              {/*{(fields, { add, remove }) => {
+                console.log(fields);
+                return fields.map(({ key, name, ...rest }) => {
+
+                    return (
+                       <Space
+                           size={"small"}
+                           key={key}
+                       >
+                           <ProFormText
+                               {...rest}
+                               name={[name, "label"]}
+                           />
+                           <ProFormDependency
+                               name={[["label"]]}
+
+                           >
+                               {(depValue) => {
+                                   console.log(depValue);
+                                   return (
+                                       <span>{JSON.stringify(depValue)}</span>
+                                   )
+                               }}
+                           </ProFormDependency>
+                       </Space>
+                    )
+              });
+            }}*/}
+          </ProFormList>
+        </ProFormItem>
+      </ProForm>
     </div>
   );
 }
